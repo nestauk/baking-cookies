be78ea5bb36dc2c4ff1953e29dc6e6f9799dd290
George Richardson <georgerichardson@posteo.net>
2019-07-03 09:59:41 +0100

    Setup Nesta Data Science cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='baking_cookies',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Tutorials and examples for working with data at Nesta.',
+    author='Nesta',
+    license='MIT',
+)
